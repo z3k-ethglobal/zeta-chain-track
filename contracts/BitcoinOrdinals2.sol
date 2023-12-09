d527a4ca32d837a364b8540b7a03ab5ded9b6f83
Prince Singhal <0xprinc@gmail.com>
2023-12-10 01:39:33 +0530

    renamed contracts

--- contracts/BitcoinOrdinals2.sol
+++ contracts/BitcoinOrdinals2.sol
@@ -5,29 +5,28 @@ import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
 import "@zetachain/protocol-contracts/contracts/zevm/interfaces/zContract.sol";
 import "@zetachain/toolkit/contracts/BytesHelperLib.sol";
 import {Base64} from "./Base64.sol";
-import "@zetachain/protocol-contracts/contracts/zevm/ZRC20.sol";
 
 contract BitcoinOrdinals2 is zContract, Base64{
 
     address public immutable NFT;
-    uint public immutable polygonChain;
+    uint public immutable polygonChainID;
     address mainNFT;
+    bytes[13] public eoa;
+    IZRC20 public constant polygonZRC20 = IZRC20(0x48f80608B672DC30DC7e3dbBd0343c5F02C738Eb);
 
     SystemContract public immutable systemContract;
 
     uint public lastToken;
 
-    mapping(uint256 => info) public desc;
-
-        struct info {
-        bytes data;                   // metadata
-    }
-
-    constructor(address systemContractAddress, address nft, address _polygonChain) {
+    constructor(address systemContractAddress, address nft, uint _polygonChainID) {
         systemContract = SystemContract(systemContractAddress);
         NFT = nft;
-        polygonChain = _polygonChain;
+        polygonChainID = _polygonChainID;
     
+    }
+
+    function set(bytes[13] memory a) public {
+        eoa = a;
     }
 
     modifier onlySystem() {
@@ -38,6 +37,12 @@ contract BitcoinOrdinals2 is zContract, Base64{
         _;
     }
 
+    function bytes4toUint(bytes4 bbs) public pure returns(uint){
+        bytes memory zz= abi.encode(bbs);
+        bytes32 kk= bytes32(zz)>>224;
+        return uint(kk);
+    }
+
     function onCrossChainCall(
         zContext calldata context,
         address zrc20,
@@ -46,9 +51,10 @@ contract BitcoinOrdinals2 is zContract, Base64{
     ) external virtual override onlySystem {
         require(context.chainID == 18332);
         for(uint i = 0; i < 13; i++){
-
-            ZRC20(systemContract.gasCoinZRC20ByChainId(polygonChain)).withdraw(mainNFT, amount);
+            bytes4 b = bytes4(abi.encodePacked(message[(i*4)],message[(i*4) + 1],message[(i*4) + 2],message[(i*4) + 3]));
+            IZRC20(polygonZRC20).withdraw(eoa[i],bytes4toUint(b));
         }
     }
+
 
 }
