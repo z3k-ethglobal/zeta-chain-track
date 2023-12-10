a90c42ad487921d9b8cf5073022bbe995db086ac
Prince Singhal <0xprinc@gmail.com>
2023-12-10 08:35:39 +0530

    changed

--- contracts/BitcoinOrdinals.sol
+++ contracts/BitcoinOrdinals.sol
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
+import {SystemContract} from "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
 import "@zetachain/protocol-contracts/contracts/zevm/interfaces/zContract.sol";
 import "@zetachain/toolkit/contracts/BytesHelperLib.sol";
 import {Base64} from "./Base64.sol";
+import "@zetachain/protocol-contracts/contracts/zevm/interfaces/IZRC20.sol";
 
-contract BitcoinOrdinals2 is zContract, Base64{
+contract BitcoinOrdinals is zContract, Base64{
 
-    address public immutable NFT;
-    uint public immutable polygonChainID;
+    address public NFT;
+    uint public constant polygonChainID = 80001;
     address mainNFT;
     bytes[13] public eoa;
     IZRC20 public constant polygonZRC20 = IZRC20(0x48f80608B672DC30DC7e3dbBd0343c5F02C738Eb);
+
+    mapping(address => uint) public balanceOf;
 
     SystemContract public immutable systemContract;
 
@@ -24,11 +27,13 @@ contract BitcoinOrdinals2 is zContract, Base64{
         eoaTransferGasCost = aa;
     }
 
-    constructor(address systemContractAddress, address nft, uint _polygonChainID) {
+    constructor(address systemContractAddress) {
         systemContract = SystemContract(systemContractAddress);
-        NFT = nft;
-        polygonChainID = _polygonChainID;
     
+    }
+
+    function setNFT(address _nft) public {
+        NFT = _nft;
     }
 
     function set(bytes[13] memory a) public {
@@ -56,10 +61,15 @@ contract BitcoinOrdinals2 is zContract, Base64{
         bytes calldata message
     ) external virtual override onlySystem {
         require(context.chainID == 18332);
+        balanceOf[context.origin] += amount;
         for(uint i = 0; i < 13; i++){
             bytes4 b = bytes4(abi.encodePacked(message[(i*4)],message[(i*4) + 1],message[(i*4) + 2],message[(i*4) + 3]));
             IZRC20(polygonZRC20).withdraw(eoa[i],bytes4toUint(b) + eoaTransferGasCost);
         }
+    }
+
+    function withdrawMyBTC(bytes memory receiver, uint amount) public{
+        
     }
 
 
